045ea040dbac81bcad9a5327e828e786a9edb2a9
Zachary Lincoln (personal) <zachlincoln@zlincoln-mac-mini.lan>
2024-12-21 00:27:07 -0500

    added cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from "@astrojs/tailwind";
 // Import the Vercel adapter
 import vercel from '@astrojs/vercel';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://positivustheme.vercel.app",
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare(),
 });
